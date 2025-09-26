a901d76c113e3e0713c1e03965053e81fc98b861
Kel Soberano <96864212+lAineDuCouteau@users.noreply.github.com>
2025-09-26 19:40:14 +0800

    Header - created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,17 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import Home from './app/Home/Home'
 import AboutMe from './app/AboutMe/AboutMe'
 import Globe from './app/Globe/Globe'
+import Header from './app/Header/Header'
 
 function App() {
   return (
     <Router>
       <div className='App'>
+        <Header />
+
         <Routes>
 
           <Route path="/" element={<Home />} />
-
           <Route path="/about" element={<AboutMe />} />
           <Route path="/globe" element={<Globe />} />
 
---
+++ frontend/src/app/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        Kelly Saguid Soberano
+      </div>
+
+      <nav className={styles.nav}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Me</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
